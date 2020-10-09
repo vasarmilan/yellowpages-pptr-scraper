72e9c5456c596e82989684b26a89442aeac5ef6e
vasarmilan <vasarmilan@gmail.com>
2020-10-09 20:30:05 +0200

    multiple changes

--- src/main.js
+++ src/main.js
@@ -1,27 +1,40 @@
 const Apify = require('apify');
-const { handleStart, handleList, handleDetail } = require('./routes');
+const { URL } = require('url');
+const routes = require('./routes');
 
 const { utils: { log } } = Apify;
 
 Apify.main(async () => {
-    const { searchTerms, geoLocationTerms } = await Apify.getInput();
-
+    const { searchTerms, geoLocationTerms, maxPages } = await Apify.getInput();
     log.info(`Search terms: ${searchTerms}, Geolocation terms: ${geoLocationTerms}`);
 
-    const requestList = await Apify.openRequestList('start-urls', []);
+    const baseURL = 'https://www.yellowpages.com';
+    const startUrl = new URL('search', baseURL);
+    startUrl.searchParams.append('search_terms', searchTerms);
+    startUrl.searchParams.append('geo_location_terms', geoLocationTerms);
+
+    const requestList = await Apify.openRequestList('start-url', [{
+        url: startUrl.href,
+        userData: { label: 'SEARCHPAGE' },
+    }]);
+
     const requestQueue = await Apify.openRequestQueue();
-    const proxyConfiguration = await Apify.createProxyConfiguration();
+    // const proxyConfiguration = await Apify.createProxyConfiguration();
 
     const crawler = new Apify.PuppeteerCrawler({
         requestList,
         requestQueue,
-        proxyConfiguration,
+        // proxyConfiguration,
         useSessionPool: true,
         persistCookiesPerSession: true,
         launchPuppeteerOptions: {
             stealth: true,
         },
         handlePageFunction: async (context) => {
+            const { request } = context;
+            const { userData: { label } } = request;
+            log.debug(`Scraping URL ${request.url} with label ${label}`);
+            await routes[label](context, { requestQueue, baseURL, maxPages });
         },
     });
 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,39 @@
 const Apify = require('apify');
 
-const { utils: { log } } = Apify;
+const { utils: { log, enqueueLinks } } = Apify;
+const { URL } = require('url');
 
-exports.handleStart = async ({ request, page }) => {
-    // Handle Start URLs
+exports.SEARCHPAGE = async ({ request, page }, { requestQueue, baseUrl, maxPages }) => {
+    log.info(`Enqueuing links from ${request.url}`);
+    log.info(JSON.stringify(await page.$eval('a.business-name', (el) => el.href)));
+    await enqueueLinks({
+        page,
+        requestQueue,
+        baseUrl,
+        selector: 'a.business-name',
+        transformRequestFunction: (req) => {
+            req.userData.label = 'LISTING';
+            return req;
+        },
+    });
+    // only enqueue the next page, if current is < maxPages
+    const currentPage = new URL(request.url).searchParams.get('page');
+    if (currentPage < maxPages || maxPages === 0) {
+        await enqueueLinks({
+            page,
+            requestQueue,
+            baseUrl,
+            selector: '.next.ajax-page',
+            transformRequestFunction: (req) => {
+                req.userData.label = 'SEARCHPAGE';
+                return req;
+            },
+        });
+    } else {
+        log.info(`Reached page ${maxPages}, skipping next page...`);
+    }
 };
 
-exports.handleList = async ({ request, page }) => {
-    // Handle pagination
-};
+exports.LISTING = async ({ request, page}, { requestQueue }) => {
 
-exports.handleDetail = async ({ request, page }) => {
-    // Handle details
-};
+}
